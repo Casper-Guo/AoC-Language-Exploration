6f78aaf60a25b9a2ad9db3f696a497481b618565
Casper Guo <casperg@umich.edu>
2023-12-08 00:39:10 -0500

    2023 day 8, pretty easy but with a MAJOR assumption re: cycles

---
+++ 2023.py/day8.py
@@ -0,0 +1,49 @@
+import regex as re
+from math import lcm
+
+def process_line(line):
+    capture = re.findall(r'[1-9A-Z]{3}', line)
+    return capture
+
+
+def move_one(current, steps, instruction, network):
+    if instruction[steps % len(instruction)] == 'L':
+        return network[current][0]
+    else:
+        return network[current][1]
+
+
+def main():
+    with open("input8.txt", "r") as f:
+        lines = f.readlines()
+        lines = [line.strip() for line in lines]
+
+    instruction = lines[0]
+    lines = list(map(process_line, lines[2:]))
+    network = {line[0]:(line[1], line[2]) for line in lines}
+
+    current = 'AAA'
+    steps = 0
+
+    # part 1
+    while current != 'ZZZ':
+        current = move_one(current, steps, instruction, network)
+        steps += 1
+
+    print(steps)
+
+    # part 2
+    ghost_start = [line[0] for line in lines if line[0].endswith('A')]
+    num_steps = []
+
+    for start in ghost_start:
+        steps = 0
+        while not start.endswith('Z'):
+            start = move_one(start, steps, instruction, network)
+            steps += 1
+        num_steps.append(steps)
+
+    print(lcm(*num_steps))
+    return
+
+main()
--- README.md
+++ README.md
@@ -31,7 +31,7 @@ Progress: 50/50 stars (100%)
 Progress: 24/50 stars (48%)
 
 ### 2023
-Progress: 14/14 stars (100%)
+Progress: 16/16 stars (100%)
 
 ## The Benches
 
