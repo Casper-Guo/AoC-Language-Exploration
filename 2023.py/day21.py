cd3ab51be5985c81ebf265aba6674ed55ca7bd47
Casper Guo <casperg@umich.edu>
2023-12-22 18:00:19 -0500

    2023 day 21 part 1 solution

---
+++ 2023.py/day21.py
@@ -0,0 +1,39 @@
+import regex as re
+import itertools
+import more_itertools as more
+from collections import defaultdict, Counter, deque
+import utils
+from math import prod
+
+
+def reachable_garden_plots(grid, current):
+    return [i for i in grid.get_dir_neighbors(current) if grid[i] == '.']
+
+
+def main():
+    with open("input21.txt", "r") as f:
+        lines = f.readlines()
+        lines = [line.strip() for line in lines]
+        lines = [[i for i in line] for line in lines]
+    
+    grid = utils.Grid(lines)
+    start = grid.find('S')[0]
+    grid[start] = '.'
+
+    # part 1
+    reached = set([start])
+    steps = 0
+    step_limit = 64
+
+    while steps < step_limit:
+        next_step = set()
+        for plot in reached:
+            next_step.update(reachable_garden_plots(grid, plot))
+
+        reached = next_step
+        steps += 1
+    
+    print(len(reached))
+    return
+
+main()
