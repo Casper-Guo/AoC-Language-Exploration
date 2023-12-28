873a67b73bfe6edd82054a5baa3beb6ae0069cea
Casper Guo <casperg@umich.edu>
2023-12-28 16:13:31 -0500

    Reduce unnecessary constructor calls

--- 2023.py/day14.py
+++ 2023.py/day14.py
@@ -6,7 +6,7 @@ def let_roll(line: list[str]) -> list[str]:
     for initial, i in enumerate(line):
         if i == "O":
             for final in range(initial - 1, -1, -1):
-                if line[final] == "O" or line[final] == "#":
+                if line[final] != '.':
                     line[initial] = "."
                     line[final + 1] = "O"
                     break
@@ -26,22 +26,22 @@ def calc_load(line: list[str]) -> int:
 def cycle(grid):
     # roll north
     new_lines = [let_roll(grid.get_col(i)) for i in range(grid.cols)]
-    grid = utils.Grid(new_lines)
+    grid.grid = new_lines
     grid.transpose()
 
     # roll west
     new_lines = [let_roll(grid.get_row(i)) for i in range(grid.rows)]
-    grid = utils.Grid(new_lines)
+    grid.grid = new_lines
 
     # roll south
     new_lines = [let_roll(list(reversed(grid.get_col(i)))) for i in range(grid.cols)]
-    grid = utils.Grid(new_lines)
+    grid.grid = new_lines
     grid.rotate_left_90()
 
     # roll east
     new_lines = [let_roll(list(reversed(grid.get_row(i)))) for i in range(grid.rows)]
     new_lines = [list(reversed(line)) for line in new_lines]
-    grid = utils.Grid(new_lines)
+    grid.grid = new_lines
 
     return grid
 
@@ -59,6 +59,7 @@ def main():
         grid = cycle(grid)
 
     print(sum(map(calc_load, [grid.get_col(i) for i in range(grid.cols)])))
+
     return
 
 
