e8dbe5505c606da32f7709043bddb12a84181fd8
Casper Guo <casperg@umich.edu>
2024-12-02 15:42:24 -0500

    Dumb n squared solution
    One optimization is to identify the first problematic/unsafe index
    and then check the three potential removals around it

---
+++ 2024.rb/day2.rb
@@ -0,0 +1,29 @@
+lines = File.readlines('input2.txt', chomp: true)
+lines = lines.map { |line| line.split.map(&:to_i) }
+
+def safety_check(report)
+  return 1 if report.length == 1
+
+  increasing = report[0] < report[1]
+
+  report.each_cons(2) do |level1, level2|
+    return 0 unless (1..3).include?((level1 - level2).abs)
+
+    return 0 if increasing && level1 > level2
+    return 0 if !increasing && level1 < level2
+  end
+  1
+end
+
+def tolerant_safety_check(report)
+  return 1 if report.length <= 2 || safety_check(report) == 1
+
+  (0...report.length).each do |i|
+    return 1 if safety_check(report[0...i] + report[(i + 1)..]) == 1
+  end
+
+  0
+end
+
+puts lines.map { |line| safety_check(line) }.sum
+puts lines.map { |line| tolerant_safety_check(line) }.sum
--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Attempting Advent of Code 2015 - 2024 using a different language for each year.
 
-Progress: 222/500 stars (44.4%)
+Progress: 224/500 stars (44.8%)
 
 ## Starting Lineup
 
@@ -48,7 +48,7 @@ Todo: optimize day 12, 14, 16, 17, 22
 
 ![Ruby](https://img.shields.io/badge/ruby-%23CC342D.svg?style=for-the-badge&logo=ruby&logoColor=white)
 
-Progress: 2/50 stars (4%)
+Progress: 4/50 stars (8%)
 
 ## The Benches
 
