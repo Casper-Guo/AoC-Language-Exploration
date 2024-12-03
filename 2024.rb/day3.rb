b17c9b90caf153566291606fb419817e09e42dfa
Casper Guo <casperg@umich.edu>
2024-12-03 11:39:05 -0500

    Cleaned up. More regex, less conditional

--- 2024.rb/day3.rb
+++ 2024.rb/day3.rb
@@ -1,26 +1,8 @@
 file = File.read('input3.txt')
-matches1 = file.scan(/mul\(([0-9]{1,3}),([0-9]{1,3})\)/)
-result1 = 0
+result1 = file.scan(/mul\(([0-9]{1,3}),([0-9]{1,3})\)/).sum { |num1, num2| num1.to_i * num2.to_i }
 
-matches1.each do |match|
-  result1 += match.map(&:to_i).inject(&:*)
-end
-
-result2 = 0
-matches2 = file.scan(/do\(\)|don't\(\)|mul\([0-9]{1,3},[0-9]{1,3}\)/)
-enabled = true
-matches2.each do |match|
-  case match
-  when /do\(\)/
-    enabled = true
-  when /don't\(\)/
-    enabled = false
-  else
-    # guarenteed that there is only to be one match
-    # so access the first element
-    result2 += enabled ? match.scan(/mul\(([0-9]{1,3}),([0-9]{1,3})\)/)[0].map(&:to_i).inject(&:*) : 0
-  end
-end
+cleaned = file.gsub(/don't\(\).*?do\(\)/m, '')
+result2 = cleaned.scan(/mul\(([0-9]{1,3}),([0-9]{1,3})\)/).sum { |num1, num2| num1.to_i * num2.to_i }
 
 puts result1
 puts result2
