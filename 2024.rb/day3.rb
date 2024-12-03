7181b1541e658a46ee47434cd8b64496c48961ef
Casper Guo <casperg@umich.edu>
2024-12-03 12:10:42 -0500

    Handle a previously uncaught edge case

--- 2024.rb/day3.rb
+++ 2024.rb/day3.rb
@@ -1,7 +1,7 @@
 file = File.read('input3.txt')
 result1 = file.scan(/mul\(([0-9]{1,3}),([0-9]{1,3})\)/).sum { |num1, num2| num1.to_i * num2.to_i }
 
-cleaned = file.gsub(/don't\(\).*?do\(\)/m, '')
+cleaned = file.gsub(/don't\(\).*?do\(\)/m, '').gsub(/don't\(\).*?$/, '')
 result2 = cleaned.scan(/mul\(([0-9]{1,3}),([0-9]{1,3})\)/).sum { |num1, num2| num1.to_i * num2.to_i }
 
 puts result1
