4d918198a40c94af60cfcb9553fdd65fb82f7a7f
Casper Guo <casperg@umich.edu>
2024-12-06 15:01:52 -0500

    Turns out not deduplicating is a lot faster

--- 2024.rb/day7.rb
+++ 2024.rb/day7.rb
@@ -1,29 +1,28 @@
 require_relative 'utils'
-require 'set'
 
 equations = File.readlines('input7.txt').map { |line| ints(line) }
 
 def solve_part1(equation)
   target = equation[0]
-  results = Set[equation[1]]
+  results = [equation[1]]
 
   equation[2...equation.length].each do |num|
     add = results.map { |result| result + num }
     mul = results.map { |result| result * num }
-    results = (add + mul).to_set
+    results = add + mul
   end
   results.include?(target) ? target : 0
 end
 
 def solve_part2(equation)
   target = equation[0]
-  results = Set[equation[1]]
+  results = [equation[1]]
 
   equation[2...equation.length].each do |num|
     add = results.map { |result| result + num }
     mul = results.map { |result| result * num }
     combine = results.map { |result| result * 10**(Math.log10(num).floor + 1) + num }
-    results = (add + mul + combine).to_set
+    results = add + mul + combine
   end
   results.include?(target) ? target : 0
 end
