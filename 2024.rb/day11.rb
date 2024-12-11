7049cff74e1a780ba759a81f61224c485bd1e6c1
Casper Guo <casperg@umich.edu>
2024-12-11 09:11:06 -0500

    Day 11, we have different definitions for memoization

---
+++ 2024.rb/day11.rb
@@ -0,0 +1,30 @@
+require_relative 'utils'
+
+stones = ints(File.read('input11.txt', chomp: true))
+stones_freq = Hash.new(0)
+stones.map { |stone| stones_freq[stone] += 1 }
+
+def blink(stone)
+  if stone.zero?
+    [1]
+  elsif ((Math.log10(stone).floor + 1) % 2).zero?
+    str = stone.to_s
+    [str[...str.length / 2], str[str.length / 2..]].map(&:to_i)
+  else
+    [stone * 2024]
+  end
+end
+
+def blink_all(stones_freq)
+  new_freq = Hash.new(0)
+  stones_freq.each_pair do |stone, freq|
+    blink(stone).map { |new_stone| new_freq[new_stone] += freq }
+  end
+  new_freq
+end
+
+75.times do
+  stones_freq = blink_all(stones_freq)
+end
+
+puts stones_freq.values.sum
--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Attempting Advent of Code 2015 - 2024 using a different language for each year.
 
-Progress: 240/500 stars (48%)
+Progress: 242/500 stars (48.4%)
 
 ## Starting Lineup
 
@@ -48,7 +48,7 @@ Todo: optimize day 12, 14, 16, 17, 22
 
 ![Ruby](https://img.shields.io/badge/ruby-%23CC342D.svg?style=for-the-badge&logo=ruby&logoColor=white)
 
-Progress: 20/50 stars (40%)
+Progress: 22/50 stars (44%)
 
 ## The Benches
 
