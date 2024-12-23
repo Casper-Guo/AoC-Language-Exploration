c6de5c7b0e373c462a80758c6679c579316bd38b
Casper Guo <casperg@umich.edu>
2024-12-22 20:15:50 -0500

    Clean, old-fashioned Ruby

---
+++ 2024.rb/day22.rb
@@ -0,0 +1,42 @@
+require 'set'
+
+seeds = File.readlines('input22.txt', chomp: true).map(&:to_i)
+sales = Hash.new(0)
+secret_sum = 0
+
+def next_secret(secret)
+  secret = (secret ^ (secret << 6)) & 0xFFFFFF
+  secret = (secret ^ (secret >> 5)) & 0xFFFFFF
+  (secret ^ (secret << 11)) & 0xFFFFFF
+end
+
+seeds.each do |seed|
+  seen = Set.new
+  diffs = 0
+
+  4.times do
+    old_price = seed % 10
+    seed = next_secret(seed)
+    new_price = seed % 10
+    # adjust range from (-9, 9) to (0, 18)
+    diffs = (diffs << 5) + (new_price - old_price + 9)
+  end
+
+  seen << diffs
+  sales[diffs] += seed % 10
+
+  1996.times do
+    old_price = seed % 10
+    seed = next_secret(seed)
+    new_price = seed % 10
+
+    # take lowest 15 bits
+    diffs = ((diffs & 0x7FFF) << 5) + (new_price - old_price + 9)
+    sales[diffs] += new_price unless seen.include?(diffs)
+    seen << diffs
+  end
+  secret_sum += seed
+end
+
+puts secret_sum
+puts sales.values.max
--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Attempting Advent of Code 2015 - 2024 using a different language for each year.
 
-Progress: 260/500 stars (52%)
+Progress: 263/500 stars (52.6%)
 
 ## Starting Lineup
 
@@ -48,7 +48,7 @@ Todo: optimize day 12, 14, 16, 17, 22
 
 ![Ruby](https://img.shields.io/badge/ruby-%23CC342D.svg?style=for-the-badge&logo=ruby&logoColor=white)
 
-Progress: 40/50 stars (80%)
+Progress: 43/50 stars (86%)
 
 Todo: optimize day 6
 
