7a0655b4b8893d15ad11a0dbe2930b1d2ab4b933
Casper Guo <casperg@umich.edu>
2025-12-02 00:06:42 -0600

    Lots of 'fun' things you can do with Scala

---
+++ 2025.scala/day01.scala
@@ -0,0 +1,65 @@
+//> using scala 3.7.4
+//> using options -deprecation -feature -unchecked -Wunused:all
+//> using plugin org.wartremover:::wartremover:3.4.1
+//> using options -P:wartremover:traverser:org.wartremover.warts.Null
+//> using options -P:wartremover:traverser:org.wartremover.warts.Return
+//> using options -P:wartremover:traverser:org.wartremover.warts.AsInstanceOf
+//> using options -P:wartremover:traverser:org.wartremover.warts.IsInstanceOf
+//> using options -P:wartremover:traverser:org.wartremover.warts.StringPlusAny
+//> using options -P:wartremover:traverser:org.wartremover.warts.OptionPartial
+//> using options -P:wartremover:traverser:org.wartremover.warts.TryPartial
+//> using options -P:wartremover:traverser:org.wartremover.warts.EitherProjectionPartial
+//> using options -P:wartremover:traverser:org.wartremover.warts.IterableOps
+// //> using options -P:wartremover:traverser:org.wartremover.warts.Throw
+//> using options -P:wartremover:traverser:org.wartremover.warts.Var
+//> using options -P:wartremover:traverser:org.wartremover.warts.MutableDataStructures
+//> using options -P:wartremover:traverser:org.wartremover.warts.While
+
+import java.lang.Math.floorMod
+
+def part1(input: List[Int], start: Int = 50): Int =
+  val (_, result) = input.foldLeft((start, 0)) {
+    case ((current, count), instruction) =>
+      val new_pos = floorMod(current + instruction, 100)
+      (new_pos, if new_pos == 0 then count + 1 else count)
+  }
+  result
+
+def part2(input: List[Int], start: Int = 50): Int =
+  val (_, result) = input.foldLeft((start, 0)) {
+    case ((current, count), instruction) =>
+      val new_pos      = floorMod(current + instruction, 100)
+      val current_zero = if new_pos == 0 then 1 else 0
+      val traverse_zeros = instruction <= 0 match
+        case true =>
+          // new_pos > current implies at least one wrap around
+          // except in the special case of starting at 0
+          if new_pos > current && current != 0 then 1 + (instruction.abs / 100)
+          else instruction.abs / 100
+        case false =>
+          // new_pos < current implies at least one wrap around
+          // except in the special case of ending at 0
+          if new_pos < current && new_pos != 0 then 1 + (instruction.abs / 100)
+          else instruction.abs / 100
+
+      // one last corner case of starting and ending on zero
+      // in which we double count
+      val total_zeros = if current == 0 && new_pos == 0 then
+        current_zero + traverse_zeros - 1
+      else
+        current_zero + traverse_zeros
+      (new_pos, count + total_zeros)
+  }
+  result
+
+@main def day01(): Unit =
+  val input = scala.io.Source.fromFile("input01.txt").getLines().toList.map {
+    _.toVector match {
+      case 'L' +: rest => -rest.mkString.toInt
+      case 'R' +: rest => rest.mkString.toInt
+      case _           => throw MatchError("Unexpected input")
+    }
+  }
+
+  println(part1(input))
+  println(part2(input))
--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Attempting Advent of Code 2015 - 2024 using a different language for each year.
 
-Progress: 270/524 stars (51.5%)
+Progress: 272/524 stars (51.9%)
 
 ## Starting Lineup
 
@@ -54,7 +54,7 @@ Todo: optimize day 6
 ### 2025
 ![Scala](https://img.shields.io/badge/scala-%23DC322F.svg?style=for-the-badge&logo=scala&logoColor=white)
 
-Progress: 0/24 stars (0%)
+Progress: 2/24 stars (8.3%)
 
 ## The Benches
 ![C#](https://img.shields.io/badge/c%23-%23239120.svg?style=for-the-badge&logo=csharp&logoColor=white)
